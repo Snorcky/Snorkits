1229816ee5e95a13d10ee7e58e97858c84890578
Snorcky <snorcky.lylouille@gmail.com>
2017-12-21 00:42:35 +0100

    Ajout des kits + commandes

--- Snorkits/src/fr/snorcky/snorkits/commands/CommandKits.java
+++ Snorkits/src/fr/snorcky/snorkits/commands/CommandKits.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import fr.snorcky.snorkits.Main;
 
@@ -15,6 +16,7 @@ public class CommandKits implements CommandExecutor {
 		this.main = main;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 			if(sender instanceof Player) {
@@ -22,19 +24,28 @@ public class CommandKits implements CommandExecutor {
 				
 				if(cmd.getName().equalsIgnoreCase("kit")) {
 					
-					if(args.length == 0) {
+					if(args.length == 0 || args.length > 1) {
 						player.sendMessage("§c/kit <nomdukit>");
 						player.sendMessage("§c/kits : Afficher la liste des kits");
 					}
 					if(args.length == 1) {
-						player.sendMessage("caca");
 						for(String kit : main.getConfig().getConfigurationSection("kits").getKeys(false)) {
-							player.sendMessage(kit);
-							System.out.println((main.getConfig().getConfigurationSection("kits").getIntegerList(kit + ".items")));
-							player.getInventory().addItem();
+							if(args[0].equalsIgnoreCase(kit)) {
+								player.sendMessage("§eVous avez reçu le kit : "+ kit);
+								for(int i : main.getConfig().getConfigurationSection("kits").getIntegerList(kit + ".items")) {
+									if(player.getInventory().firstEmpty() == -1) { //Si l'inventaire du joueur est plein
+										player.getWorld().dropItem(player.getPlayer().getLocation(), new ItemStack(i, 1)); //on drop l'item au sol
+										player.sendMessage(("Votre inventaire est plein, votre achat est à vos pieds!"));
+									}else {
+										player.getInventory().addItem(new ItemStack(i, 1)); // on lui donne l'item
+									}
+								}
+							}
+							if(!args[0].equalsIgnoreCase(kit)) {
+								player.sendMessage("caca");
+							}
 						}
 					}
-					
 				}
 				if(cmd.getName().equalsIgnoreCase("kits")) {
 					if(args.length == 0) {
